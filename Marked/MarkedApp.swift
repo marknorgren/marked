dd2822e042dece7467cc02bd561835f51b8286ef
Mark Norgren <mark@marknorgren.com>
2021-02-04 19:54:31 -0600

    Initial Commit

---
+++ Marked/MarkedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkedApp.swift
+//  Marked
+//
+//  Created by mark on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MarkedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
